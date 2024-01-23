6e8fe4018d1af6d4db5d2f405547353b96cec6d9
Andrew Alsing <andrew32alsing@gmail.com>
2024-01-23 13:34:04 -0800

    completed lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function PizzaForm() {
+  const [name, setName] = useState("");
+  const [pepperoni, setPepperoni] = useState(false);
+  const [sausage, setSausage] = useState(false);
+  const [peppers, setPeppers] = useState(false);
+  const [onions, setOnions] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pizza Order</h1>
+      <form>
+        <div>
+          <label>
+            <input
+              type="text"
+              value={name}
+              placeholder="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={pepperoni}
+              onChange={() => setPepperoni(!pepperoni)}
+            />
+            Pepperoni
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={sausage}
+              onChange={() => setSausage(!sausage)}
+            />
+            Sausage
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={peppers}
+              onChange={() => setPeppers(!peppers)}
+            />
+            Peppers
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={onions}
+              onChange={() => setOnions(!onions)}
+            />
+            Onions
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <h2>Your Order</h2>
+      <p>{name}</p>
+      {pepperoni && <p>Pepperoni</p>}
+      {sausage && <p>Sausage</p>}
+      {peppers && <p>Peppers</p>}
+      {onions && <p>Onions</p>}
     </div>
   );
 }
 
-export default App;
+export default PizzaForm;
